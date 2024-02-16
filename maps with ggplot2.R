fb94bc382c2a77c5a25b54ff5560d0e8d83843e0
Juan Carlos Rubio-Polania <71674994+Keyanswers@users.noreply.github.com>
2024-02-15 18:27:43 -0600

    Update maps with ggplot2.R

--- maps with ggplot2.R
+++ maps with ggplot2.R
@@ -1,14 +1,19 @@
+# Loading packages
 
 pck = c("ggplot2", "ggspatial", "sf")
 lapply(pck, require, character.only = TRUE)
 
+# Getting shapefile
 ## https://geodata.lib.berkeley.edu/?f%5Bdc_format_s%5D%5B%5D=Shapefile&f%5Bdct_spatial_sm%5D%5B%5D=Iceland
 shp = st_read(dsn = "C:/Shapefile4/ISL_adm1.shp")
 
-x11()
+
+x11() # open graphic window 
+
+# Map visualization
 ggplot(data = shp) + geom_sf() 
 
-
+# Improving the map visualization 
 ggplot(data = shp) + geom_sf() +
   coord_sf(xlim = c(-27.9, -12.5),
            ylim = c(63, 67)) +
@@ -25,6 +30,7 @@ ggplot(data = shp) + geom_sf() +
 shp_LAM = st_transform(shp, 9947)
 st_bbox(shp_LAM)
 
+# Improving the map visualization using specific projection
 ggplot(data = shp_LAM) + geom_sf() +
   coord_sf(xlim = c(1229383.1, 2050290.9),
            ylim = c(1102427.1, 1502427.1)) +
